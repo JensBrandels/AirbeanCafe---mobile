498dea04b0c6ead7944e6761c2e04da57667ef85
JensBrandels <brandelsjens@gmail.com>
2024-03-04 16:02:34 +0100

    AboutPage + NavPage styling DONE

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -1,5 +1,52 @@
+import HeaderTop from "../../assets/headertop.svg";
+import BarIcon from "../../assets/Bar-icon.png";
+import OwnerImg from "../../assets/Owner-img.png";
+import FooterImg from "../../assets/footer.svg";
+import "./aboutPage.scss";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="AboutPage-container">
+      <div className="AboutPage-topImages">
+        <img src={BarIcon} alt="" className="positionfix" />
+        <img src={HeaderTop} alt="" />
+      </div>
+      <article className="AboutPage-text">
+        <h1>Vårt Kaffe</h1>
+        <p className="fixedWeight">
+          Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio,
+          grounds dripper, crema, strong whipped, variety extra iced id lungo
+          half and half mazagran. Pumpkin spice.
+        </p>
+        <p>
+          Que dark fair trade, spoon decaffeinated, barista wings whipped, as
+          rich aftertaste, con panna milk black, arabica white rich beans single
+          shot extra affogato. So affogato macchiato sit extraction instant
+          grinder seasonal organic, turkish single shot, single origin, and
+          robusta strong to go so dripper. Viennese froth, grounds
+          caramelization skinny aromatic cup kopi-luwak, fair trade flavour,
+          frappuccino medium, café au lait flavour cultivar ut bar instant
+          kopi-luwak.
+        </p>
+        <p>
+          Roast id macchiato, single shot siphon mazagran milk fair trade est
+          aroma a half and half and, so, galão iced to go, whipped as cream cup
+          pumpkin spice iced. At extra, rich grinder, brewed to go, steamed half
+          and half at, that, percolator macchiato trifecta and body as arabica
+          dripper. In galão black java milk sit trifecta, robusta, acerbic café
+          au lait instant shop latte. Seasonal bar shop filter aroma id, crema,
+          affogato viennese cultivar aftertaste, seasonal, percolator cream
+          black, galão flavour, milk aromatic turkish skinny crema.
+        </p>
+      </article>
+      <div className="OwnerIMG">
+        <img src={OwnerImg} alt="" />
+      </div>
+      <div className="AboutPage-footerImg">
+        <img src={FooterImg} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
--- src/components/pages/NavPage.tsx
+++ src/components/pages/NavPage.tsx
@@ -1,5 +1,34 @@
+import NavIcon from "../../assets/NavIcon.png";
+
+import "./navPage.scss";
+
 const NavPage = () => {
-  return <div>NavPage</div>;
+  return (
+    <div className="NavPage-Container">
+      <div className="NavPage-iconContainer">
+        <img src={NavIcon} alt="" />
+      </div>
+      <div>
+        <ul className="NavigationContainer">
+          <li>
+            <p>Meny</p>
+          </li>
+          <hr className="lineWidth" />
+          <li>
+            <p>Vårt Kaffe</p>
+          </li>
+          <hr className="lineWidth" />
+          <li>
+            <p>Min Profil</p>
+          </li>
+          <hr className="lineWidth" />
+          <li>
+            <p>Orderstatus</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default NavPage;
