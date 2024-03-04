f458e6bd4f3001382aa0cc891412d3ec0e31ffe7
JensBrandels <brandelsjens@gmail.com>
2024-03-04 12:55:42 +0100

    landingpage styling done

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,5 +1,18 @@
+import Headerleft from "../../assets/headerleft.svg";
+import MainLogo from "../../assets/mainLogo.png";
+import Headerright from "../../assets/headerright.svg";
+import "./landingPage.scss";
+
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div className="landingPage-container">
+      <img src={Headerleft} alt="" className="landingPage-leftIMG" />
+
+      <img src={MainLogo} alt="" />
+
+      <img src={Headerright} alt="" className="landingPage-rightIMG" />
+    </div>
+  );
 };
 
 export default LandingPage;
