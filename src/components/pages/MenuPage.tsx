1d74ffecc9e834b8a42600138d27e77405b13c18
JensBrandels <brandelsjens@gmail.com>
2024-03-05 12:53:15 +0100

    base of MenuPage done

--- src/components/pages/MenuPage.tsx
+++ src/components/pages/MenuPage.tsx
@@ -1,5 +1,31 @@
+import HeaderTop from "../../assets/headertop.svg";
+import BarIcon from "../../assets/Bar-icon.png";
+import BagIcon from "../../assets/bag.svg";
+import FooterImg from "../../assets/footer.svg";
+
+import "./menuPage.scss";
+
 const MenuPage = () => {
-  return <div>MenuPage</div>;
+  return (
+    <div className="Menu-Container">
+      <div className="Menu-imgContainer">
+        <img src={BarIcon} alt="" className="barImg" />
+        <img src={HeaderTop} alt="" className="HeaderImg" />
+
+        <p className="ColorChange">7</p>
+        <div className="BagIcon-positioner">
+          <img src={BagIcon} alt="" />
+        </div>
+      </div>
+      <div>
+        <h2 className="Menu-Title">Meny</h2>
+      </div>
+      <div>Put content here</div>
+      <div className="Menu-footerImg">
+        <img src={FooterImg} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default MenuPage;
