682f16286a47e2068976183721e3a106787474e8
JensBrandels <brandelsjens@gmail.com>
2024-03-08 18:17:12 +0100

    StatusPage styling done

--- src/pages/StatusPage.tsx
+++ src/pages/StatusPage.tsx
@@ -1,6 +1,8 @@
 import { getEta } from "../api/apiFetches";
 import { useCartStore } from "../store/CartStore";
 import { useEffect, useState } from "react";
+import DroneImg from "../assets/droneImg.svg";
+import "./statusPage.scss";
 
 const StatusPage = () => {
   const { orderNumber } = useCartStore();
@@ -15,9 +17,20 @@ const StatusPage = () => {
   }, []);
 
   return (
-    <div>
-      <div>ORDERNUMBER {orderNumber}</div>
-      <div>ETA {eta}</div>
+    <div className="statusPage-container">
+      <div>
+        <p className="statusPage-text">
+          Ordernummer: <span className="fatFont">{orderNumber}</span>
+        </p>
+      </div>
+      <img src={DroneImg} />
+      <h3 className="statusPage-title">Din beställning är på väg!</h3>
+      <div>
+        <p className="statusPage-text">
+          <span className="fatFont">{eta}</span> minuter
+        </p>
+      </div>
+      <button className="statusPage-button">Ok, cool!</button>
     </div>
   );
 };
